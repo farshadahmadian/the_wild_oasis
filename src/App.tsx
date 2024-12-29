1095ec7bcf067e255c68fa365ed1867f38f4e5c0
Farshad <f.ahmadiyanyazdi@gmail.com>
2024-12-29 12:35:03 +0100

    add starter files

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>hi</H1>
+        <Button onClick={() => alert("hi")}>Click</Button>
+        <Input type="number" placeholder="number of guests" />
+      </StyledApp>
+    </div>
+  );
+}
+
+export default App;
